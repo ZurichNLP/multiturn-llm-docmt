af4700baef549a3974aab3b4e13333237a242896
Hanxu Hu <huhanxu1233@gmail.com>
2025-01-27 17:05:15 +0100

    update

--- scripts/run_gpt.sh
+++ scripts/run_gpt.sh
@@ -1,6 +1,6 @@
-TARGET_LANGS="hi"
+TARGET_LANGS="cs de es hi is ja ru uk zh"
 MODEL_NAME_PATH=gpt-4o-mini
-SETTINGS="mturn"
+SETTINGS="mturn mturn_icl seg seg_icl single single_icl"
 for setting in $SETTINGS; do
     for lang in $TARGET_LANGS; do
         bash scripts/run_trans_gpt_split.sh en $lang $setting wmt24_processed $MODEL_NAME_PATH
